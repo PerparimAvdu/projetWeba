f7f98f8220b318e6e8a40ef85392716d338d14dd
PerparimAvdu <perparim.avdll@eduge.ch>
2021-12-06 00:04:55 +0100

    Update settings.py
    Chagement settings

--- Projet_44location/settings.py
+++ Projet_44location/settings.py
@@ -16,9 +16,6 @@ from pathlib import Path
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-import requests
-username = 'PerparimAvdu'
-token = '86b9858ae3a91eed085af21d57f3cb631e5cf03c'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
